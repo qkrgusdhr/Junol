f0a08babb30c3d0ec5da39cf32497d697bf8837c
21-manic-017g <21-manic-017g@21-manic-017>
2024-03-21 13:13:45 +0900

    0321

---
+++ Jungol/src/q503/Main.java
@@ -0,0 +1,13 @@
+package q503;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int weight = 49;
+		float gravity = (float) 0.268300;
+		
+		System.out.println(weight + "*" + gravity + "=" + weight * gravity);
+
+	}
+
+}
---
+++ Jungol/src/q506/Main.java
@@ -0,0 +1,11 @@
+package q506;
+public class Main {
+	public static void main(String[] args) {
+		int heighgt = 170;
+		double weight = 68.6;
+		System.out.println("My height");
+		System.out.println(heighgt);
+		System.out.println("My weight");
+		System.out.printf("%.6f",weight);
+	}
+}
